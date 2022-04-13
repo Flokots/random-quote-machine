30ad39f686cd983d832826f5b4871cb4e8377f69
FloKots <florencekotohoyoh@gmail.com>
2022-04-13 13:27:32 +0300

    display quote in the app

--- src/app/components/quotes-display/quotes-display.component.ts
+++ src/app/components/quotes-display/quotes-display.component.ts
@@ -9,20 +9,30 @@ import { Quote } from 'src/app/model/quote';
 })
 export class QuotesDisplayComponent implements OnInit {
 
-  quote!: Quote;
+  currentQuote: String = '';
+  currentAuthor: String = '';
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(){
+  ngOnInit() {
     interface ApiResponse {
-      author: string;
-      quote: string;
+      quotes: Quote;
     }
 
-    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data => {
+    this.http.get<any>("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").subscribe(data => {
       // Successful API request
-      this.quote = new Quote (data.quote, data.author, 0, 0)
-    } )
+      let jsonQuotes = data
+      console.log(jsonQuotes);
+      console.log("above are json quotes")
+      let randomQuote: Quote = jsonQuotes.quotes[Math.floor(Math.random() * jsonQuotes.quotes.length)];
+      console.log(randomQuote)
+      console.log("I am a random quote above")
+
+      this.currentQuote = randomQuote.quote;
+      console.log(this.currentQuote);
+      this.currentAuthor = randomQuote.author;
+      console.log(this.currentAuthor);
+    })
   }
 
 }
--- src/app/model/quote.ts
+++ src/app/model/quote.ts
@@ -2,11 +2,6 @@ export class Quote {
   constructor(
     public quote: String,
     public author: String,
-    public upVotes: number,
-    public downVotes: number
-  ) { 
-    this.upVotes = 0;
-    this.downVotes = 0;
-  }
+  ) {  }
 
 }
