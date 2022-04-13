a5e86efabe91a1fe8784d01aa81359a509b95e3f
FloKots <florencekotohoyoh@gmail.com>
2022-04-13 04:30:44 +0300

    add quotes-display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesDisplayComponent } from './components/quotes-display/quotes-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesDisplayComponent
   ],
   imports: [
     BrowserModule,
