9da0ffe09a56d6a9a4c9a7031c1c507414c034f5
FloKots <florencekotohoyoh@gmail.com>
2022-04-14 05:20:03 +0300

    revert to the last working commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgProgressComponent } from 'ngx-progressbar';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
